2025c9b5cb20b3bec5c80e300dc6fc49d2117306
SUPRIYA0423 <supriuuu.04@gmail.com>
2026-03-17 23:57:22 +0530

    Add files via upload

---
+++ column_sum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class column_sum {
+    public static void main(String args[]){
+        Scanner a=new Scanner(System.in);
+        System.out.print("enter rows:");
+        int r=a.nextInt();
+        System.out.print("enter column:");
+        int c=a.nextInt();
+        int [][] m=new int[r][c];
+        System.out.print("enter elements:");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                m[i][j]=a.nextInt();
+            }
+        }
+        int sum=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                sum=sum+m[i][j];
+            }
+        }
+        System.out.println("sum of column elements is:"+sum);
+        
+
+        
+}
+}
